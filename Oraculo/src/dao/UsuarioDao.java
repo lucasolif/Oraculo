385e2fa4f956d22971044dddd15b24ac6d6aa3d6
Lucas Oliveira <62679209+lucasolif@users.noreply.github.com>
2023-09-22 01:16:22 -0300

    Add files via upload

--- Oraculo/src/dao/UsuarioDao.java
+++ Oraculo/src/dao/UsuarioDao.java
@@ -4,6 +4,7 @@ import jdbc.Conexao;
 import model.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioDao {
@@ -11,21 +12,48 @@ public class UsuarioDao {
     public void adicionarUsuario(Usuario usuario) throws SQLException{
         
         Connection conexao = new Conexao().getConexao();
-        String sqk = "insert into usuario (login,senha,codigo,nome,sobrenome,email,celular,setor) value (?,?,?,?,?,?,?,?)";
+        String sqk = "insert into usuario (login,senha,nome,sobrenome,email,celular,setor,codigo,sexo) value (?,?,?,?,?,?,?,?,?)";
         PreparedStatement ps = conexao.prepareStatement(sqk);
         
         ps.setString(1, usuario.getLogin());
         ps.setString(2, usuario.getSenha());
-        ps.setString(3, usuario.getCod());
-        ps.setString(4, usuario.getNome());
+        ps.setString(3, usuario.getNome());
         ps.setString(4, usuario.getSobrenome());
-        ps.setString(6, usuario.getEmail());
-        ps.setString(7, usuario.getCelular());
+        ps.setString(5, usuario.getEmail());
+        ps.setString(6, usuario.getCelular());
         ps.setString(7, usuario.getSetor());
+        ps.setString(8, usuario.getCodigo());
+        ps.setString(9, usuario.getSexo());
         
         ps.execute();
         ps.close();
         conexao.close();
         
+    }
+    
+    public Usuario logar(String login, String senha) throws SQLException{
+        
+        Connection conexao = new Conexao().getConexao();
+        String sql = "SELECT * FROM usuario WHERE login=? and senha=?";
+        PreparedStatement ps = Conexao.prepareStatement(sql);
+        
+        ps.setString(1, login);
+        ps.setString(2, senha);
+        
+        ResultSet rs = ps.executeQuery();
+        
+                //enquanto estiver dado na tabela
+        Usuario usuario = null;
+        while(rs.next()){
+            usuario = new Usuario();
+            usuario.setLogin(rs.getString("login"));
+            usuario.setSenha(rs.getString("senha"));
+        }
+        
+        rs.close();
+        ps.close();
+        conexao.close();
+        
+        return usuario;
     }
 }
--- Oraculo/src/jdbc/Conexao.java
+++ Oraculo/src/jdbc/Conexao.java
@@ -3,9 +3,14 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Conexao {
+
+    public static PreparedStatement prepareStatement(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
     private final String nameDataBase = "oraculo";
     private final String nameUser = "root";
